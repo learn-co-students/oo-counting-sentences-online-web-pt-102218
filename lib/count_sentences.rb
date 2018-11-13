dc26f8c4e1a5b47600d77eacfa57e4838dcf217b
grant smith <grantsmith1256@gmail.com>
2018-11-13 03:57:18 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,27 @@
-require 'pry'
+require "pry"
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split(/[.!?]/)
+     count = 0
+     array.each do |sentence|
+      if sentence.length > 1
+      count +=1
+    end
   end
+  count
+end
 end
